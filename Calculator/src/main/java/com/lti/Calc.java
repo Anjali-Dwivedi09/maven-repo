f07f442fd8a9008b3e80080cbf57d2102201bed7
Anjali-Dwivedi09 <anjalidwivedi0109@gmail.com>
2020-12-21 12:43:48 +0530

    First Commit

---
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -0,0 +1,14 @@
+package com.lti;
+
+public class Calc {
+
+	public static int add(int n, int m){
+		return (n+m);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(add(10,20));
+
+	}
+
+}
