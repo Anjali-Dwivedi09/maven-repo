d54d2c93f31d19260c981c036116b9f9eb3228b0
Anjali-Dwivedi09 <anjalidwivedi0109@gmail.com>
2020-12-21 15:17:25 +0530

    Division added

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -13,11 +13,15 @@ public class Calc {
 	public static int pro(int n, int m){
 		return (n*m);
 	}
+	public static int div(int n, int m){
+		return (n/m);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Sum= "+add(10,20));
 		System.out.println("Difference= "+sub(50,20));
 		System.out.println("Product= "+pro(50,20));
+		System.out.println("Division= "+div(10,2));
 
 	}
 
