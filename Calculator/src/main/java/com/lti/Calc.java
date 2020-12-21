ea8135448802b5032c384ab67f28fc6cef3a201f
Anjali-Dwivedi09 <anjalidwivedi0109@gmail.com>
2020-12-21 14:59:27 +0530

    Added another method

--- Calculator/src/main/java/com/lti/Calc.java
+++ Calculator/src/main/java/com/lti/Calc.java
@@ -5,9 +5,14 @@ public class Calc {
 	public static int add(int n, int m){
 		return (n+m);
 	}
+	
+	public static int sub(int n, int m){
+		return (n-m);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(add(10,20));
+		System.out.println("Sum= "+add(10,20));
+		System.out.println("Difference= "+sub(50,20));
 
 	}
 
